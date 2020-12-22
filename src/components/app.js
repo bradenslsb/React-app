374752fc368d841b39485b1b839e8221ef9380a0
bradenslsb <bbir1199@gmail.com>
2020-12-21 18:00:04 -0700

    end of the first day of code mostly understanding the frame work

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+
+import PortfolioContainer from "./portfolio/portfolio-container"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Braden Bird's Portfolio</h1>
-        <h2>React Redux Router</h2>
+        <PortfolioContainer />
+        
       </div>
     );
   }
