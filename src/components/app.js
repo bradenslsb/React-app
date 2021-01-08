af9dc503c33aa80731e1079764d8eb2791ee282d
bradenslsb <bbir1199@gmail.com>
2021-01-08 01:16:24 -0700

    This was a lot of work on the portfolio had to play a little catch up for class

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
-import moment from "moment";
+//import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
-import NavigationContainer from "./navigation/navigation-container"
+//import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact"
+import Blog from "./pages/blog"
+import PortfolioDetail from "./portfolio/portfolio-detail"
+import Auth from "./pages/auth"
+import NoMatch from "./pages/no-match"
+
+
+
 
 export default class App extends Component {
+
   render() {
     return (
-      <div className="app">
-        <NavigationContainer />
-        <h1>Braden Bird's Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
+      <div className="container">
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/protfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+
+
+            </Switch>
+          </div>
+        </Router>
+
+
+        
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
@@ -9,15 +10,10 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        { title: "Quip", category: "eCommerce" },
-        { title: "Eventbrite", category: "Scheduling" },
-        { title: "Ministry Safe", category: "Enterprise" },
-        { title: "SwingAway", category: "eCommerce" }
-      ]
+      data: []
     };
 
-    this.handleFilter = this.handleFilter.bind(this)
+    this.handleFilter = this.handleFilter.bind(this);
   }
 
   handleFilter(filter) {
@@ -28,33 +24,51 @@ export default class PortfolioContainer extends Component {
     });
   }
 
+  getPortfolioItems() {
+    axios
+      .get("https://bradenbird.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={"google.com"} />;
+      return <PortfolioItem key={item.id} item={item} />;
     });
   }
 
-  
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
 
   render() {
     if (this.state.isLoading) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
-    
-    return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
 
-        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-        <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-        <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-        <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+    return (
+      
+      <div className="portfolio-items-wrapper">
+
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
         {this.portfolioItems()}
-
-        
-
       </div>
+      
     );
   }
 }
