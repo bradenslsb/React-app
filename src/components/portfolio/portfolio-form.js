a26d9c1a3cd2741c9a654a22f993884212ea2bbf
bradenslsb <bbir1199@gmail.com>
2021-01-11 12:54:56 -0700

    Finishing up the dropzone functionality

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import axios from "axios"
 import DropzoneComponent from "react-dropzone-component"
 
 import "../../../node_modules/react-dropzone-component/styles/filepicker.css"
 import "../../../node_modules/dropzone/dist/min/dropzone.min.css"
-
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -21,11 +20,19 @@ export default class PortfolioForm extends Component {
       logo: ""
     }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.componentConfig = this.componentConfig.bind(this)
-    this.djsConfig = this.djsConfig.bind(this)
-    this.handleThumbDrop = this.handleThumbDrop.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleThumbDrop = this.handleThumbDrop.bind(this);
+    this.handleBannerDrop = this.handleBannerDrop.bind(this);
+    this.handleLogoDrop = this.handleLogoDrop.bind(this);
+
+
+    this.thumbRef = React.createRef()
+    this.bannerRef = React.createRef()
+    this.logoRef = React.createRef()
+
   }
 
   handleThumbDrop() {
@@ -34,10 +41,22 @@ export default class PortfolioForm extends Component {
     }
   }
 
+  handleBannerDrop() {
+    return {
+      addedfile: file => this.setState({ banner_image: file })
+    }
+  }
+
+  handleLogoDrop() {
+    return {
+      addedfile: file => this.setState({ logo: file })
+    }
+  }
+
   componentConfig() {
     return {
-      iconFileTypes: [".jpg", ".png"], 
-      showFileTypeIcon: true,
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
       postUrl: "https://httpbin.org/post"
     }
   }
@@ -52,16 +71,25 @@ export default class PortfolioForm extends Component {
   buildForm() {
     let formData = new FormData()
 
-    formData.append("portfolio_item[name]", this.state.name)
-    formData.append("portfolio_item[description]", this.state.description)
-    formData.append("portfolio_item[url]", this.state.url)
-    formData.append("portfolio_item[category]", this.state.category)
-    formData.append("portfolio_item[position]", this.state.position)
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
+
     if (this.state.thumb_image) {
-      formDatat.append("portfolio_item"[thumb_image], this.state.thumb_image)
+      formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
     }
 
-    return formData
+    if (this.state.banner_image) {
+      formData.append("portfolio_item[banner_image]", this.state.banner_image);
+    }
+
+    if (this.state.logo) {
+      formData.append("portfolio_item[logo]", this.state.logo);
+    }
+
+    return formData;
   }
 
   handleChange(event) {
@@ -71,34 +99,53 @@ export default class PortfolioForm extends Component {
   }
 
   handleSubmit(event) {
-    axios 
-      .post("https://bradenbird.devcamp.space/portfolio/portfolio_items", 
+    axios
+      .post(
+        "https://bradenbird.devcamp.space/portfolio/portfolio_items",
         this.buildForm(),
         { withCredentials: true }
-      ).then(response => {
-        {this.props.handleSuccessfulFormSubmission(response.data.portfolio_item)}
-      }).catch(error => {
-        console.log("portfolio form handle submit form", error)
+      )
+      .then(response => {
+        this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
+
+        this.setState({
+          name: "",
+          description: "",
+          category: "eCommerce",
+          position: "",
+          url: "",
+          thumb_image: "",
+          banner_image: "",
+          logo: ""
+        })
+
+        [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref  => {
+          ref.current.dropzone.removeAllFiles()
+        })
       })
-    
+      .catch(error => {
+        console.log("portfolio form handleSubmit error", error);
+      });
+
     event.preventDefault()
   }
 
   render() {
     return (
       <div>
-        <h1>portfolio form</h1>
+        <h1>PortfolioForm</h1>
 
         <form onSubmit={this.handleSubmit}>
           <div>
-            <input 
+            <input
               type="text"
               name="name"
               placeholder="Portfolio Item Name"
               value={this.state.name}
               onChange={this.handleChange}
             />
-            <input 
+
+            <input
               type="text"
               name="url"
               placeholder="URL"
@@ -106,14 +153,16 @@ export default class PortfolioForm extends Component {
               onChange={this.handleChange}
             />
           </div>
+
           <div>
-            <input 
+            <input
               type="text"
               name="position"
               placeholder="Position"
               value={this.state.position}
               onChange={this.handleChange}
             />
+
             <select
               name="category"
               value={this.state.category}
@@ -123,10 +172,10 @@ export default class PortfolioForm extends Component {
               <option value="Scheduling">Scheduling</option>
               <option value="Enterprise">Enterprise</option>
             </select>
-            
           </div>
+
           <div>
-            <textarea 
+            <textarea
               type="text"
               name="description"
               placeholder="Description"
@@ -136,13 +185,26 @@ export default class PortfolioForm extends Component {
           </div>
 
           <div className="image-uploaders">
-            <DropzoneComponent 
-              congif={this.componentConfig()}
-              djsCongig={this.djsConfig()}
+            <DropzoneComponent
+              ref={this.thumbRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
               eventHandlers={this.handleThumbDrop()}
-              >
+            />
 
-              </DropzoneComponent>
+            <DropzoneComponent
+              ref={this.bannerRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleBannerDrop()}
+            />
+
+            <DropzoneComponent
+              ref={this.logoRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleLogoDrop()}
+            />
           </div>
 
           <div>
