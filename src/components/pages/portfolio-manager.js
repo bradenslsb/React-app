5f19470c3e42ef41f8e319fcc4c80be7b60fef83
bradenslsb <bbir1199@gmail.com>
2021-01-10 18:35:41 -0700

    added the scss to the portfolio-manager and made the portfolio form.js doc and built out the form to start adding things to the form

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
+import axios from "axios"
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list"
+import PortfolioForm from '../portfolio/portfolio-form'
+
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      portfolioItems: []
+    }
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this)
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    //TODO
+    //update the portfolioItems state
+    //and portfolio item to the list
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("this handle error", error)
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items",{ 
+        withCredentials: true
+      }).then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        })
+      }).catch(error => {
+        console.log("error catching portfolio items", error)
+      })
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems()
+  }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <h1>Portfolio form...</h1>
+          <PortfolioForm 
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission} 
+            handleFormSubmissionError={this.handleFormSubmissionError}
+          />
+        </div>
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems}/>
         </div>
 
       </div>
